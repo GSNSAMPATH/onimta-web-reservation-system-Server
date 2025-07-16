63c749ad16386fe7ca8e8eb413d4784b63e708e5
Thamalpath <thamalpathsathimantha3@gmail.com>
2025-07-16 10:45:04 +0530

    Debug and Release files

--- obj/Debug/net8.0/OIT_Reservation.AssemblyInfo.cs
+++ obj/Debug/net8.0/OIT_Reservation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OIT_Reservation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08f89809687071a22e593b0f222360efb244337b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ccae8aff0c71f9d1914634a9fb43793aabaa2dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("OIT_Reservation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OIT_Reservation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
